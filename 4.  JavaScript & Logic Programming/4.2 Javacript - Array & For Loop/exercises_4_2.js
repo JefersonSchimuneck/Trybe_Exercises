77642d921ca6dee37a28d7f646676e880125ed2c
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2020-10-26 20:33:40 -0300

    Completes exercise 5

--- 4.  JavaScript & Logic Programming/4.2 Javacript - Array & For Loop/exercises_4_2.js
+++ 4.  JavaScript & Logic Programming/4.2 Javacript - Array & For Loop/exercises_4_2.js
@@ -13,10 +13,29 @@ let sum = 0;
 for (let index = 0; index < numbers.length; index +=1 ) {
     sum += numbers[index];
 }
-console.log(`2. A soma dos elementos do array resulta em ${sum}`)
+console.log(`2. A soma dos elementos do array resulta em ${sum}.`)
 console.log("")
 
 //3
 let average = sum / numbers.length;
-console.log(`3. A média aritmética dos elementos do array resulta em ${average}`)
+console.log(`3. A média aritmética dos elementos do array resulta em ${average}.`)
+console.log("")
+
+//4
+if (average > 20) {
+    console.log('4. Valor maior que 20.')
+} else {
+    console.log('4. Valor menor ou igual a 20.')
+}
+console.log("")
+
+//5
+let highestNumber = 0;
+for (let index = 0; index < numbers.length; index +=1 ) {
+    if (numbers[index] > highestNumber){
+        highestNumber = numbers[index];
+    }
+}
+
+console.log(`5. O maior número do array é ${highestNumber}.`)
 console.log("")
