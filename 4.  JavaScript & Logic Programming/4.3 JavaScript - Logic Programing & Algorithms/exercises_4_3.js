6736aaf8126ac57e83b8523e3d6ca319058a3433
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2020-10-27 18:53:16 -0300

    Complete exercises 1 and 2

---
+++ 4.  JavaScript & Logic Programming/4.3 JavaScript - Logic Programing & Algorithms/exercises_4_3.js
@@ -0,0 +1,20 @@
+//1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n.
+let n = 10;
+let asteriskLine = "";
+console.log("1.");
+for (let indexLine = 0; indexLine < n; indexLine +=1) {
+        asteriskLine += "*";
+}
+for (let indexColumn = 0; indexColumn < n; indexColumn +=1) {
+    console.log(asteriskLine);
+}
+console.log();
+
+//2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
+asteriskLine = "";
+console.log("2.");
+for (let indexLine = 0; indexLine < n; indexLine +=1) {
+    asteriskLine += "*";
+    console.log(asteriskLine);
+}
+console.log();
