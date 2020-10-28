b4a3b7fb145e70aeb42b85c81c78c8b2f0a84c45
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2020-10-27 21:31:32 -0300

    Complete exercise 4

--- 4.  JavaScript & Logic Programming/4.3 JavaScript - Logic Programing & Algorithms/exercises_4_3.js
+++ 4.  JavaScript & Logic Programming/4.3 JavaScript - Logic Programing & Algorithms/exercises_4_3.js
@@ -1,5 +1,5 @@
 //1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n.
-let n = 10;
+let n = 9;
 let asteriskLine = "";
 console.log("1.");
 for (let indexLine = 0; indexLine < n; indexLine +=1) {
@@ -21,16 +21,58 @@ console.log();
 
 //3- Agora inverta o lado do triângulo
 console.log("3.");
-let c = 1;
+let counter = 1;
 for (let indexLine = 0; indexLine < n; indexLine +=1) {
+    //Reseta a string a cada início de loop
     asteriskLine = "";
-    for (let indexSpace = n-c; indexSpace >= 0; indexSpace -=1) {
+    //Começa preenchendo os primeiros caracteres com espaço e vai reduzinfo a cada loop conforme o contador
+    for (let indexSpace = n-counter; indexSpace >= 0; indexSpace -=1) {
             asteriskLine += " ";
     }
-    for (let indexAsterix = n+c; indexAsterix > n; indexAsterix -=1) {
+    //Começa preenchendo os últimos caracteres com asterisco e vai aumentando a cada loop conforme o contador
+    for (let indexAsterisk = n+counter; indexAsterisk > n; indexAsterisk -=1) {
         asteriskLine += "*";    
     }
+    //Imprime linha
     console.log(asteriskLine);
-    c += 1;
+    //Incrementa contador
+    counter += 1;
+}
+console.log();
+
+//4- Depois, faça uma pirâmide com n asteriscos de base:
+console.log("4.");
+let lineCounter = 0;
+let counterSpace = 0;
+let counterAsterisk = 0;
+//Condicional, que faz o código executar somente para números ímpares
+if (n % 2 != 0) {
+    //Calcula o número de linhas que a pirâmide terá e serve de controle para posicionamento dos elementos
+    for (let index = n; index > 0; index -=2) {
+        lineCounter += 1;
+    }
+    //Loop que percorre o número de linhas
+    for (let indexLine = lineCounter; indexLine > 0; indexLine -=1){
+        //Reseta a string a cada início de loop
+        asteriskLine = "";
+        //Preenche os primeiros caracteres com espaço antes do meio e vai reduzindo conforme o contador de espaços
+        for (let indexSpace = (n-lineCounter) - counterSpace; indexSpace >= 0; indexSpace -=1) {
+            asteriskLine += " ";
+        }
+        //Preenche o meio com asteriscos e vai incrementando conforme o contador de asteriscos
+        for (let indexAsterisk = (lineCounter+1) + counterAsterisk; indexAsterisk > lineCounter; indexAsterisk -=1) {
+            asteriskLine += "*";    
+        }//Mesma ideia do preenchimento dos primeiros espaços
+        for (let indexSpace = (n-lineCounter) - counterSpace; indexSpace >= 0; indexSpace -=1) {
+            asteriskLine += " ";
+        }
+        //Imprime a string
+        console.log(asteriskLine);
+        //Incrementa contadores
+        counterSpace += 1;
+        counterAsterisk +=2;
+    }
+} else {
+    console.log("Favor digitar um número ímpar");
 }
 console.log();
