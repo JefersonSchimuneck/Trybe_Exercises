b2a35216673e01d20403920bf9f2f1383787d5a9
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2020-11-16 15:51:35 -0300

    Exercises 6.1 6.2

--- 6. HTML & CSS: Forms, Flexbox & Responsive/6.1 HTML & CSS-Forms/script.js
+++ 6. HTML & CSS: Forms, Flexbox & Responsive/6.1 HTML & CSS-Forms/script.js
@@ -36,52 +36,72 @@ for (let index = 0; index < states.length; index += 1) {
   statesField.appendChild(newState);
 }
 
-// const startDate = document.getElementById("start-date");
-// function verifyDate() {
-//   startDate.addEventListener("change", function () {
-//     let date = startDate.value;
-//     if (date.length < 10) {
-//       console.log(date[3] + date[4]);
-//     } else if (date[0] + date[1] < 1 || date[0] + date[1] > 31) {
-//       console.log(date[0] + date[1]);
-//     } else if (date[3] + date[4] < 1 || date[3] + date[4] > 12) {
-//       console.log(date[3] + date[4]);
-//     } else if (date[6] + date[7] + date[8] + date[9] < 0) {
-//       console.log(date[6] + date[7] + date[8] + date[9]);
-//     } else if (date[2] != "/" || date[5] != "/") {
-//       alert("erro");
-//     }
-//   });
-// }
-// verifyDate();
+const startDate = document.getElementById("start-date");
+
+function verifyDate() {
+  startDate.addEventListener("change", function () {
+    let date = startDate.value;
+    let error = 0;
+    if (date.length < 10) {
+error +=1;
+    } else if (date[0] + date[1] < 1 || date[0] + date[1] > 31) {
+      error +=1;
+    } else if (date[3] + date[4] < 1 || date[3] + date[4] > 12) {
+      error +=1;
+    } else if (date[6] + date[7] + date[8] + date[9] < 0) {
+      error +=1;
+    } else if (date[2] != "/" || date[5] != "/") {
+      error +=1;
+    }
+    return error;
+  });
+}
+verifyDate()
 
 const submitButton = document.getElementById("submit");
 const inputFields = document.querySelectorAll("input");
 const resumeField = document.querySelector(".resume");
 
+// function verifyFilled() {
+//   for (let index = 0; index < inputFields.length; index += 1) {
+//     if (inputFields[index].type == "text") {
+//       if ((inputFields[index].value = "")) {
+//         console.log('teste')
+//       }
+//     }
+//   }
+// }
+
 submitButton.addEventListener("click", function (event) {
   event.preventDefault();
-  for (let index = 0; index < inputFields.length; index += 1) {
-    if (inputFields[index].type == "text") {
-      let printInfo = document.createElement("p");
-      printInfo.innerHTML =inputFields[index].name + " " + inputFields[index].value;
-      resumeField.appendChild(printInfo);
-    }
+  if (verifyDate() > 0) {
+    alert('teste')
   }
-});
+})
+  
+// });
 
-const clearButton = document.getElementById("clear");
+// function printResume() {
+//   for (let index = 0; index < inputFields.length; index += 1) {
+//     if (inputFields[index].type == "text") {
+//       let printInfo = document.createElement("p");
+//       printInfo.innerHTML =
+//         inputFields[index].name + " " + inputFields[index].value;
+//       resumeField.appendChild(printInfo);
+//     }
+//   }
+// }
 
-clearButton.addEventListener("click", function (event) {
-  clear()
-  event.preventDefault();
-});
+// function clear() {
+//   const printedResume = document.querySelectorAll("p");
+//   for (let index = 0; index < printedResume.length; index += 1) {
+//     resumeField.removeChild(printedResume[index]);
+//     inputFields[index].value = "";
+//   }
+// }
 
- function clear() {
-  const printedResume = document.querySelectorAll("p");
-  for (let index = 0; index < printedResume.length; index += 1) {
-      resumeField.removeChild(printedResume[index]);
-      inputFields[index].value = "";    
-  }
- }
-
+// const clearButton = document.getElementById("clear");
+// clearButton.addEventListener("click", function (event) {
+//   clear();
+//   event.preventDefault();
+// });
