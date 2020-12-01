f641a292d531805d7c68de1d3f3f00321fc04cf8
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2020-12-01 20:11:52 -0300

    Complete exercise 6

--- 8. JavaScript-Higher Order Functions/8.2 HOF-map & filter/exercises_8_2.js
+++ 8. JavaScript-Higher Order Functions/8.2 HOF-map & filter/exercises_8_2.js
@@ -287,3 +287,16 @@ function fantasyOrScienceFictionAuthors() {
 }
 
 assert.deepEqual(fantasyOrScienceFictionAuthors(), expectedResultE);
+
+
+const expectedResultF = [
+  'O Senhor dos Anéis',
+  'Fundação',
+  'O Chamado de Cthulhu'
+]
+// 6 Crie um array com o nome de todos os livros com mais de 60 anos de publicação.
+function oldBooksB() {
+   return booksB.filter((book) => 2020 - book.releaseYear > 60).map((book) => book.name);
+}
+
+assert.deepEqual(oldBooksB(), expectedResultF);
