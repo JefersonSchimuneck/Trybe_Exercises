7d69947d1c4c7349397bdb37a46a86ec478d7dc5
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2020-12-02 18:34:41 -0300

    Complete exercise 1

---
+++ 8. JavaScript-Higher Order Functions/8.3 HOF - reduce/exercises_8_3.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+
+//1 Dada uma matriz de matrizes, transforme em uma única matriz.
+const arrays = [
+    ["1", "2", "3"],
+    [true],
+    [4, 5, 6]
+];
+
+
+function flatten() {
+  return arrays.reduce((accumulator, array) => accumulator.concat(array));
+}
+
+assert.deepStrictEqual(flatten(), ["1", "2", "3", true, 4, 5, 6]);
