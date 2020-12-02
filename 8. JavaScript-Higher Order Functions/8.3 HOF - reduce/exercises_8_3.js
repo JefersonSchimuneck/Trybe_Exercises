85f28af427594b36693a48d973cef8a6ee8f7a2b
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2020-12-02 20:58:54 -0300

    Complete exercise 5

--- 8. JavaScript-Higher Order Functions/8.3 HOF - reduce/exercises_8_3.js
+++ 8. JavaScript-Higher Order Functions/8.3 HOF - reduce/exercises_8_3.js
@@ -111,9 +111,31 @@ const expectedResultD = {
         if (longestTitle.name.length > currentTitle.name.length){
             return longestTitle;
         }
-        return currentTitle
+        return currentTitle;
     })
   }
   
   assert.deepStrictEqual(longestNamedBook(), expectedResultD);
+
+
+  //5 Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
+  const names = [
+    'Aanemarie',  'Adervandes',   'Akifusa',
+    'Abegildo',   'Adicellia',    'Aladonata',
+    'Abeladerco', 'Adieidy',  'Alarucha',
+  ];
+  
+  
+  
+  function containsA() {
+    const concatNames = names.reduce((accumulator, name) => accumulator.concat(name.split('')), []);
+    return concatNames.reduce((acc, letter) => {
+      if (letter === 'a' || letter === 'A') {
+        acc +=1;
+      }
+      return acc;
+    }, 0);   
+  }
+  
+  assert.deepStrictEqual(containsA(), 20);
   
