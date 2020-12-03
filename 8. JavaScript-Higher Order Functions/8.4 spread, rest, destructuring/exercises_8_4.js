480dd2d71bb20073faad4ebd80efa45aac4b30de
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2020-12-03 19:40:24 -0300

    Complete exercise 7

--- 8. JavaScript-Higher Order Functions/8.4 spread, rest, destructuring/exercises_8_4.js
+++ 8. JavaScript-Higher Order Functions/8.4 spread, rest, destructuring/exercises_8_4.js
@@ -106,3 +106,28 @@ const toObject = ([name, brand, year]) => ({ name, brand, year });
 assert.deepStrictEqual(toObject(palio), { name: "Palio", brand: "Fiat", year: 2019 })
 assert.deepStrictEqual(toObject(shelbyCobra), { name: "Shelby Cobra", brand: "Ford", year: 1963 })
 assert.deepStrictEqual(toObject(chiron), { name: "Chiron", brand: "Bugatti", year: 2016 })
+
+
+//7 Escreva uma função shipLength que, dado um objeto representando um navio, retorna o comprimento dele, mostrando também a devida unidade de comprimento:
+const ships = [
+    {
+        name: "Titanic",
+        length: 269.1
+    },
+    {
+        name: "Queen Mary 2",
+        length: 1132,
+        measurementUnit: "feet"
+    },
+    {
+        name: "Yamato",
+        length: 256,
+        measurementUnit: "meters"
+    }
+]
+
+const shipLength = ({name, length, measurementUnit = "meters"}) => `${name} is ${length} ${measurementUnit} long`;
+
+assert.strictEqual(shipLength(ships[0]), "Titanic is 269.1 meters long")
+assert.strictEqual(shipLength(ships[1]), "Queen Mary 2 is 1132 feet long")
+assert.strictEqual(shipLength(ships[2]), "Yamato is 256 meters long")
