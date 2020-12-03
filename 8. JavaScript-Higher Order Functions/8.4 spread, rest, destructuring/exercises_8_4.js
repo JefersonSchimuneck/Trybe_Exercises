dc7b5f448c3300c24ec5e68a973d48fc632f98c3
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2020-12-03 18:43:53 -0300

    Complete exercise 2

--- 8. JavaScript-Higher Order Functions/8.4 spread, rest, destructuring/exercises_8_4.js
+++ 8. JavaScript-Higher Order Functions/8.4 spread, rest, destructuring/exercises_8_4.js
@@ -11,3 +11,13 @@ const rectangles = [rectangle1, rectangle2, rectangle3]
 for(rectangle of rectangles) {
     assert.strictEqual(rectangleArea(...rectangle), rectangle[0]*rectangle[1]) // altere a chamada da funcao rectangleArea
 }
+
+//2 Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos. Ou seja:
+
+const sum = (...numbers) => numbers.reduce((acc, number) => acc + number, 0);
+
+assert.strictEqual(sum(), 0)
+assert.strictEqual(sum(1), 1)
+assert.strictEqual(sum(1, 2), 3)
+assert.strictEqual(sum(1, 2, 3), 6)
+assert.strictEqual(sum(1, 2, 3, 4), 10)
