8bee15f0f1ce139e8177703622f55675f4ea0a0f
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2020-12-03 18:48:24 -0300

    Complete exercise 3

--- 8. JavaScript-Higher Order Functions/8.4 spread, rest, destructuring/exercises_8_4.js
+++ 8. JavaScript-Higher Order Functions/8.4 spread, rest, destructuring/exercises_8_4.js
@@ -1,6 +1,6 @@
 const assert = require('assert')
 
-// Dado o código abaixo, complete-o de forma que seja impressa a área dos 3 retângulos. O código deve retornar em sequência 2 , 15 e 54 .
+//1 Dado o código abaixo, complete-o de forma que seja impressa a área dos 3 retângulos. O código deve retornar em sequência 2 , 15 e 54 .
 const rectangleArea = (width, height) => width * height
 
 const rectangle1 = [1, 2]
@@ -13,7 +13,6 @@ for(rectangle of rectangles) {
 }
 
 //2 Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos. Ou seja:
-
 const sum = (...numbers) => numbers.reduce((acc, number) => acc + number, 0);
 
 assert.strictEqual(sum(), 0)
@@ -21,3 +20,23 @@ assert.strictEqual(sum(1), 1)
 assert.strictEqual(sum(1, 2), 3)
 assert.strictEqual(sum(1, 2, 3), 6)
 assert.strictEqual(sum(1, 2, 3, 4), 10)
+
+//3 Escreva a função personLikes , que dado um objeto de parâmetro que representa uma pessoa, retorna todos os gostos daquela pessoa, conforme mostrado abaixo:
+const alex = {
+    name: "Alex",
+    age: 26,
+    likes: ["fly fishing"],
+    nationality: "Australian"
+}
+
+const gunnar = {
+    name: "Gunnar",
+    age: 30,
+    likes: ["hiking", "scuba diving", "taking pictures"],
+    nationality: "Icelandic"
+}
+
+// complete a assinatura da função abaixo
+const personLikes = ({name, age, likes}) => `${name} is ${age} years old and likes ${likes.join(", ")}.`
+assert.strictEqual(personLikes(alex), "Alex is 26 years old and likes fly fishing.")
+assert.strictEqual(personLikes(gunnar), "Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.")
