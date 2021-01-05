88e06bac76b3bc27617ead488ffc088f26c133c3
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2021-01-05 18:58:01 -0300

    Complete exercise 2

--- 10. Test automation - Jest/10.2 Jest - Asynchoronous Tests/exercise_10_2.test.js
+++ 10. Test automation - Jest/10.2 Jest - Asynchoronous Tests/exercise_10_2.test.js
@@ -9,3 +9,41 @@ test(`'trybe' to uppercase 'TRYBE'`, done => {
       done();
     });
 });
+
+//2
+const users = {
+  4: { name: 'Mark' },
+  5: { name: 'Paul' }
+};
+
+const findUserById = (id) => {
+  return new Promise((resolve, reject) => {
+      if (users[id]) {
+        return resolve(users[id]);
+      }
+
+      return reject({ error: 'User with ' + id + ' not found.' });
+  });
+}
+
+const getUserName = (userID) => {
+  return findUserById(userID).then(user => user.name);
+}
+
+describe('test getUserName with promises', () => {
+  describe('when ID exists', () => {
+    it('returns the user name', () => {
+      expect.assertions(1);
+      return getUserName(4).then(data => expect(data).toEqual('Mark'));
+    });
+  });
+
+  describe('when ID doesnt exist', () => {
+    it('returns an error', () => {
+      expect.assertions(1);
+      return getUserName(2).catch(error =>
+        expect(error).toEqual({ error: 'User with 2 not found.' })
+      );
+    });
+  });
+});
