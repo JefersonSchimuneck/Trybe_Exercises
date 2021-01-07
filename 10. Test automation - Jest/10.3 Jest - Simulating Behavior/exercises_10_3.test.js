0efe4fc5141a227998caae64da08171c64cf1fdb
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2021-01-06 21:11:07 -0300

    Complete exercise 6

--- 10. Test automation - Jest/10.3 Jest - Simulating Behavior/exercises_10_3.test.js
+++ 10. Test automation - Jest/10.3 Jest - Simulating Behavior/exercises_10_3.test.js
@@ -49,3 +49,37 @@ module.exports = {
     firstCharacter,
     concatString,
   };
+
+  //6
+function fetchDog() {
+    return fetch("https://dog.ceo/api/breeds/image/random").then(response =>
+      response
+        .json()
+        .then(json =>
+          response.ok ? Promise.resolve(json) : Promise.reject(json)
+        )
+    );
+  }
+
+describe("testando a requisição", () => {
+  fetchDog = jest.fn();
+  afterEach(fetchDog.mockReset);
+
+  test("testando requisição caso a promisse resolva", async () => {
+    fetchDog.mockResolvedValue("request sucess");
+
+    fetchDog();
+    expect(fetchDog).toHaveBeenCalled();
+    expect(fetchDog).toHaveBeenCalledTimes(1);
+    expect(fetchDog()).resolves.toBe("request sucess");
+    expect(fetchDog).toHaveBeenCalledTimes(2);
+  });
+
+  test("testando requisição caso a promisse seja rejeitada", async () => {
+    fetchDog.mockRejectedValue("request failed");
+
+    expect(fetchDog).toHaveBeenCalledTimes(0);
+    expect(fetchDog()).rejects.toMatch("request failed");
+    expect(fetchDog).toHaveBeenCalledTimes(1);
+  });
+});
