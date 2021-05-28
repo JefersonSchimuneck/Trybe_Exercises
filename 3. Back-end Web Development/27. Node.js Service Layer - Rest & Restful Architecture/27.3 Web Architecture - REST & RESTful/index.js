faba7f40e1392ac91a4dd4c046d31182e6004e1d
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2021-05-28 19:09:41 -0300

    Complete exercises 27.3

---
+++ 3. Back-end Web Development/27. Node.js Service Layer - Rest & Restful Architecture/27.3 Web Architecture - REST & RESTful/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/products', require('./controllers/productController'))
+
+app.listen(3000, () => console.log('App listening on port 3000'));
